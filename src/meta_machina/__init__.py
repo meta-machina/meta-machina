d4feb61030eaca77c50c5650955177d41acc3482
Alex Fedotov <alxfed@gmail.com>
2024-05-04 10:43:12 -0500

    resource

--- src/meta_machina/__init__.py
+++ src/meta_machina/__init__.py
@@ -6,7 +6,7 @@ This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
 import os
-import githf
+from . import githf
 import yaml
 import ruamel.yaml as ryaml
 
